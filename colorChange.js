56503661eca4f439dec0b201771dea472c19fccc
Adarsh Kumar Pal <paladarsh978@gmail.com>
2024-09-01 13:37:36 +0530

    add new file

---
+++ colorChange.js
@@ -0,0 +1,27 @@
+        // let a1 = document.querySelector("#a1");
+        // let a2 = document.querySelector("#a2");
+        // let btn = document.querySelector("#a3");
+
+        // let check = 0;
+        // btn.addEventListener("click", () => {
+        //     if (check ==0){
+
+        //         a1.innerHTML = "I like MT";
+        //         a2.innerHTML = "I love MT";
+
+        //         check = 1;
+        //     }else{
+        //         a1.innerHTML = "I love MT";
+        //         a2.innerHTML = "I like MT";
+        //         check = 0;
+        //     }
+        // })
+
+
+let a1 = document.querySelector("#a1");
+let a2 = document.querySelector("#a2");
+let a3 = document.querySelector("#a3");
+
+a3.addEventListener("click", () => {
+    a2.innerText = "<h1>hellow adarsh click me</h1>";
+})
